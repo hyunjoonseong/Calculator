a74966d1e259b97fc80a81e0e87b88aef0019256
hyunjoonseong <j97w96@naver.com>
2022-12-25 22:00:07 +0900

    while

--- 01.cpp
+++ 01.cpp
@@ -2,53 +2,76 @@
 
 using namespace std;
 
-int main() {
+
+int main()
+{
+
+
 
     int num = 0; // 숫자를 입력받을 변수 선언.
-    int value = 0; // 계산한 값을 받아줄 변수 선언.
-    int val = 0;
-    char a; // 연산자를 받을 변수 선언.
-
-    cin >> num; // 첫번째 숫자 입력받기.
-    value = num; // 반복문에 들어가기 전 첫번째 값을 담아두기.
-    val = num; // n을 눌렀을 때 초기값을 예비로 담아놓는 변수.
-    
-    char yn;
-    int i = 0;
-    while (i<100)
+    char a = 0; // 연산자를 받을 변수 선언.
+    int value = 0; // 합을 입력받을 변수 선언.
+    char yn; //Close를 입력받을 변수 선언.
+    while (1)
     {
-        cin >> a;
-        if (a == '=')
+            cin >> num;
+            value = num;
+           
+        while (1)
         {
-            cout << value << endl;
-            cout << "Close? y or n" << endl;
-            cin >> yn;
-            if (yn == 'y') 
-                break;     // y를 눌렀을 때 프로그램 종료
-            if (yn == 'n') 
-                value = val;// n을 눌렀을 때 value의 값을 새로 초기화.
-                i = 0;
-          
-         
+
+
+            cin >> a;
+            if (a != '=')
+            {
+                cin >> num; // 첫번째 숫자 입력받기.
+            }
+
+            if (a == '+')
+            {
+                value += num;
+            }
+            else if (a == '-')
+            {
+                value -= num;
+            }
+            else if (a == '*')
+            {
+                value *= num;
+            }
+            else if (a == '/')
+            {
+                value /= num;
+            }
+            else if (a == '%')
+            {
+                value %= num;
+            }
+            else if (a == '=')
+            {
+                cout << value << endl;
+                cout << "Close? y or n" << endl;
+                cin >> yn;
+                if (yn == 'y')
+                {
+                    break;     // y를 눌렀을 때 프로그램 종료
+                }
+                if (yn == 'n')
+                {
+                    
+                    break;
+                }
+            }
+
         }
-            
-
-        cin >> num;
-        if (a == '+')
-            value += num;
-        else if (a == '-')
-            value -= num;
-        else if (a == '*')
-            value *= num;
-        else if (a == '/')
-            value /= num;
-        else if (a == '%')
-            value %= num;
-        i++;
+        if (yn == 'y')
+        {
+            break;     // y를 눌렀을 때 프로그램 종료
+        }
     }
-  
+    return 0;
+}
 
 
-        return 0;
-    }
+
 
